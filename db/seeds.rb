543d5b49abef477c1120fd6a429d9feef8f7fe11
Ben Hanisch <benjamin.hanisch@gmail.com>
2020-05-26 13:50:43 -0500

    commit in a while

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Benjamin", password: "password")
+User.create(username: "Jacquelyn", password: "password")
+User.create(username: "Guinevere", password: "password")
+
+
+Message.create(body: "Hi, I am a new messagge", user: User.last)
+Message.create(body: "Wasssup", user: User.first)
+Message.create(body: "Hello to you again", user: User.last)
+Message.create(body: "Good Morning to all!!!", user: User.find(2))
